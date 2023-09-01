e9e2349e8d06b0a0033030467eeb9e5f5350e057
natybastazini <natybastazini7@gmail.com>
2023-09-01 12:23:58 -0300

    precisa finalizargit add .

---
+++ aula05/app.js
@@ -0,0 +1,7 @@
+/************************************************************
+ * Objetivo: Realizar o calculo de uma determinada tabuada.
+ * Data: 31/08/2023. 
+ * Autor: Natalia Bastazini.
+ * Versao: 1.0
+ ************************************************************/
+
---
+++ aula05/modulo/calculoTabuada.js
@@ -0,0 +1,57 @@
+/************************************************************
+ * Objetivo: Arquivo de funções para calcular uma tabuada.
+ * Data: 31/08/2023. 
+ * Autor: Natalia Bastazini.
+ * Versao: 1.0
+ ************************************************************/
+
+//Função para calcular uma tabuada.
+const calcularTabuada = function(valorTabuada, contadorInicial, contadorFinal){
+    //Entradas de dados que chegam pelos argumentos.
+    let tabuada = String(valorTabuada).replace(',','.')
+    let contInicial = String(contadorInicial).replace(',','.')
+    let contFinal = String(contadorFinal).replace(',','.')
+
+    let resultado
+    let status = false
+
+    // let valorAuxiliar
+
+    //Validação para organizar os valores (menor e maior)
+    if (contFinal < contInicial){
+            // valorAuxiliar = contInicial
+            // valorAuxiliar = contInicial
+            // contInicial = contFinal
+            // contFinal = valorAuxiliar
+
+        contFinal = contadorInicial
+        contInicial = contadorFinal
+    }
+        
+    if (tabuada == '' || contInicial == '' || contFinal == '')
+        console.log('ERRO: É obrigatório a entrada de todos os valores!!')
+    else if (isNaN(tabuada) || isNaN(contInicial) || isNaN(contFinal))
+        console.log('ERRO: É obrigatório a entrada somente de números')
+    else{
+        //Converte as variáveis em número.
+        contInicial = Number(contInicial)
+        contFinal = Number(contFinal)
+        tabuada = Number(tabuada)
+
+        //Exemplo usando o WHILE.
+        while(contInicial <= contFinal){
+            resultado = tabuada * contInicial
+            // console.log(tabuada + 'x' + contInicial + '=' + resultado)
+            console.log(`${ tabuada} x ${contInicial} = ${resultado}`)
+            // contInicial = contInicial + 1
+            contInicial++
+            // contInicial+=1
+
+            status = true
+        }
+    }
+
+    return status
+}
+
+console.log(calcularTabuada(7,0,10))
